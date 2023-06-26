788500e873b48a03fa0478342fa003d695a10c27
Anddrrew <59182007+Anddrrew@users.noreply.github.com>
2023-06-26 21:09:05 +0300

    Improved UI of StartCard

--- src/components/StartCard.tsx
+++ src/components/StartCard.tsx
@@ -4,6 +4,7 @@ import {
   CardBody,
   CardFooter,
   CardHeader,
+  Center,
   FormControl,
   FormHelperText,
   FormLabel,
@@ -19,14 +20,17 @@ import {
   Stack,
 } from '@chakra-ui/react';
 import { useGame } from '../providers/GameProvider';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import GameMode from '../types/GameMode';
+import { FireIcon } from '@heroicons/react/24/outline';
 
 export default function StartCard() {
   const { startGame } = useGame();
   const [mode, setMode] = useState(GameMode.PLAYER);
   const [n, setN] = useState(12);
   const [m, setM] = useState(3);
+
+  useEffect(() => setM(Math.min(2 * n + 1, m)), [n]);
 
   const handleStart = () => startGame(mode, n, m);
 
@@ -36,9 +40,12 @@ export default function StartCard() {
         <Heading size='md'>Matchsticks</Heading>
       </CardHeader>
       <CardBody>
-        <Stack gap={3}>
+        <Stack gap={4}>
+          <Center w='60'>
+            <FireIcon />
+          </Center>
           <FormControl>
-            <FormLabel>First Move</FormLabel>
+            <FormLabel>Game Mode</FormLabel>
             <RadioGroup value={mode} onChange={(v) => setMode(v as GameMode)}>
               <HStack>
                 <Radio value={GameMode.PLAYER}>Player</Radio>
@@ -48,7 +55,7 @@ export default function StartCard() {
           </FormControl>
 
           <FormControl>
-            <FormLabel>N</FormLabel>
+            <FormLabel>n</FormLabel>
             <NumberInput value={n} min={1} onChange={(v) => setN(Number(v))} isRequired>
               <NumberInputField />
               <NumberInputStepper>
@@ -60,15 +67,15 @@ export default function StartCard() {
           </FormControl>
 
           <FormControl>
-            <FormLabel>M</FormLabel>
-            <NumberInput value={m} min={1} onChange={(v) => setM(Number(v))} isRequired>
+            <FormLabel>m</FormLabel>
+            <NumberInput value={m} min={1} max={2 * n + 1} onChange={(v) => setM(Number(v))} isRequired>
               <NumberInputField />
               <NumberInputStepper>
                 <NumberIncrementStepper />
                 <NumberDecrementStepper />
               </NumberInputStepper>
             </NumberInput>
-            <FormHelperText>Can take 1 to M matches at a time</FormHelperText>
+            <FormHelperText>Can take 1 to m matches at a time</FormHelperText>
           </FormControl>
         </Stack>
       </CardBody>
