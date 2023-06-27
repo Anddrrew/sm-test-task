ca3fe0ccfb248f87635ad35a711870740c8462df
Anddrrew <59182007+Anddrrew@users.noreply.github.com>
2023-06-27 21:14:49 +0300

    Refactored take function in GameProvider

--- src/providers/GameProvider.tsx
+++ src/providers/GameProvider.tsx
@@ -26,40 +26,6 @@ export default function GameProvider({ children }: Props) {
     if (!game?.isUserTurn) botTakeMatches();
   }, [game]);
 
-  const botTakeMatches = () => {
-    setGame((game) => {
-      if (game) {
-        const { availableMatches, playerMatches, botMatches, maxTake } = game;
-
-        const take = getQuantityTake(availableMatches, playerMatches, botMatches, maxTake);
-
-        return {
-          ...game,
-          availableMatches: availableMatches - take,
-          botMatches: botMatches + take,
-          isUserTurn: true,
-          maxTake: getNewMaxTake(availableMatches, take, maxTake),
-        };
-      }
-    });
-  };
-
-  const takeMatches = (take: number) => {
-    setGame((game) => {
-      if (game) {
-        const { availableMatches, playerMatches, maxTake } = game;
-
-        return {
-          ...game,
-          availableMatches: availableMatches - take,
-          playerMatches: playerMatches + take,
-          isUserTurn: false,
-          maxTake: getNewMaxTake(availableMatches, take, maxTake),
-        };
-      }
-    });
-  };
-
   const startGame = (mode: GameMode, n: number, m: number) => {
     setGame({
       mode,
@@ -73,9 +39,35 @@ export default function GameProvider({ children }: Props) {
     setStatus(GameStatus.RUNNING);
   };
 
+  const updateGame = (cb: (game: IGame) => IGame) => setGame((game) => (game ? cb(game) : game));
+
   const endGame = () => setStatus(GameStatus.FINISHED);
 
   const resetGame = () => setStatus(GameStatus.IDLE);
+
+  const takeMatches = (take: number) => updateGame((game) => handleTake(game, take));
+
+  const botTakeMatches = () => {
+    updateGame((game) => {
+      const { availableMatches, playerMatches, botMatches, maxTake } = game;
+      const take = getQuantityTake(availableMatches, playerMatches, botMatches, maxTake);
+
+      return handleTake(game, take);
+    });
+  };
+
+  const handleTake = (game: IGame, take: number) => {
+    const { availableMatches, botMatches, playerMatches, maxTake, isUserTurn } = game;
+
+    return {
+      ...game,
+      availableMatches: availableMatches - take,
+      botMatches: !isUserTurn ? botMatches + take : botMatches,
+      playerMatches: isUserTurn ? playerMatches + take : playerMatches,
+      isUserTurn: !isUserTurn,
+      maxTake: getNewMaxTake(availableMatches, take, maxTake),
+    };
+  };
 
   return (
     <GameContext.Provider value={{ status, game, takeMatches, startGame, endGame, resetGame }}>
