074b2b204fedb36542f85e0c26fa8e3941dedcbe
Anddrrew <59182007+Anddrrew@users.noreply.github.com>
2023-06-28 21:22:09 +0300

    Updated getQuantityTake

--- src/utils/getQuantityTake.ts
+++ src/utils/getQuantityTake.ts
@@ -1,6 +1,7 @@
 /* Quantity choosing algorithm.
- * I have an opinion that it can be improved for a more intelligent version of the bot.
- * Perhaps should try to count the number of player matches when the availableMatches approach the maxTake intersection
+ * It should be improved for a more intelligent version of the bot.
+ * idea 1: try to count the number of player and bot matches when the availableMatches approach the maxTake intersection
+ * idea 2: minimax
  */
 
 export default function getQuantityTake(
@@ -10,13 +11,6 @@ export default function getQuantityTake(
   maxTake: number
 ) {
   if (availableMatches === 1) return availableMatches;
-
-  if (availableMatches <= maxTake) {
-    if (availableMatches % 2 === 0) {
-      return botMatches % 2 === 0 ? availableMatches : availableMatches - 1;
-    }
-    return botMatches % 2 === 0 ? availableMatches - 1 : availableMatches;
-  }
 
   if (maxTake % 2 === 0) {
     return botMatches % 2 === 0 ? maxTake : maxTake - 1;
